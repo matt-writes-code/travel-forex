3db388919a13f3a82f68e9dfd4445bc7478e066c
mayojich <mayojich@gmail.com>
2022-09-24 10:33:44 +0800

    tky

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import MalaysianCounter from './components/malaysian-counter'
-import Forex from './components/forex'
+import MalaysianCounter from "./components/malaysian-counter";
+import Forex from "./components/forex";
 import "./styles.css";
 
-const App=()=> {
-
-const [data, setData]=useState()
-const callback=(x)=>{
-  setData(x)
-}
+const App = () => {
+  const [data, setData] = useState();
+  const callback = (x) => {
+    setData(x);
+  };
 
   return (
     <div className="App">
       <h1 className="title">Travel Forex</h1>
-      <MalaysianCounter slot={callback}/>
-      <Forex currency="IDR" date="Nov 2019" rate="0.0002975" myr={data}/>
-      <Forex currency="THB" date="Aug 2019" rate="0.134" myr={data}/>
-      <Forex currency="JPY" date="Dec 2018" rate="0.03663" myr={data}/>
-      <Forex currency="SGD" date="Sep 2018" rate="3.00" myr={data}/>
-      <br/>
+      <MalaysianCounter slot={callback} />
+      <Forex currency="TRY" date="Sep 2022" rate="0.2605" myr={data} />
+      <Forex currency="IDR" date="Nov 2019" rate="0.0002975" myr={data} />
+      <Forex currency="THB" date="Aug 2019" rate="0.134" myr={data} />
+      <Forex currency="JPY" date="Dec 2018" rate="0.03663" myr={data} />
+      <Forex currency="SGD" date="Sep 2018" rate="3.00" myr={data} />
+      <br />
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
