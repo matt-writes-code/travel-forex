2cf2e9a04108d25ca666156948cad7919f00960d
mayojich <mayojich@gmail.com>
2022-09-24 11:53:19 +0800

    .

--- src/components/forex.js
+++ src/components/forex.js
@@ -1,16 +1,19 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-const Forex=({currency, date, rate, myr})=>{
+const Forex = ({ currency, date, rate, myr }) => {
+  const valuation = myr / rate;
 
-const valuation = (myr / rate)
+  return (
+    <>
+      <div className="forex">
+        <p className="valuation">
+          {currency}: {valuation ? valuation.toFixed(2) : "0"}
+        </p>
+        <p>Exchange rate: {rate}</p>
+        <p>{date}</p>
+      </div>
+    </>
+  );
+};
 
-  return(<>
-<div className="forex">
-<p className="valuation">{currency}: { valuation ? valuation.toFixed(2) : "0"}</p>
-<p>Exchange rate: {rate}</p>
-<p>{date}</p>
-</div>
-  </>)
-}
-
-export default Forex
+export default Forex;
